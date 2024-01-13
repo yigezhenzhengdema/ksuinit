7f1f315693c81e7dd4db7f39661e35e0d0154bc6
Ylarod <me@ylarod.cn>
2024-01-13 15:28:52 +0800

    remove missing_symbols

--- src/loader.rs
+++ src/loader.rs
@@ -50,65 +50,29 @@ pub fn load_module(path: &str) -> Result<()> {
     let mut buffer = fs::read(path).with_context(|| format!("Cannot read file: {path}"))?;
     let elf = Elf::parse(&buffer)?;
 
-    let missing_symbols = vec![
-        "kallsyms_lookup_name",
-        "register_kprobe",
-        "dentry_path_raw",
-        "kern_path",
-        "path_put",
-        "unregister_kprobe",
-        "security_add_hooks",
-        "groups_free",
-        "groups_alloc",
-        "groups_sort",
-        "set_groups",
-        "path_umount",
-        "init_nsproxy",
-        "security_hook_heads",
-        "filp_open",
-        "kernel_read",
-        "kernel_write",
-        "strncpy_from_user_nofault",
-        "security_secid_to_secctx",
-        "security_secctx_to_secid",
-        "selinux_state",
-        "selinux_blob_sizes",
-        "symtab_search",
-        "symtab_insert",
-        "ebitmap_set_bit",
-        "ebitmap_get_bit",
-        "avtab_search_node",
-        "avtab_search_node_next",
-        "avtab_insert_nonunique",
-        "policydb_filenametr_search",
-        "__hashtab_insert",
-        "avc_ss_reset",
-        "selnl_notify_policyload",
-        "selinux_status_update_policyload",
-    ];
-
     let kernel_symbols = parse_kallsyms()?;
 
     let mut modifications = Vec::new();
     for (index, mut sym) in elf.syms.iter().enumerate() {
+        if index == 0 {
+            continue;
+        }
+
+        if sym.st_shndx != section_header::SHN_UNDEF as usize {
+            continue;
+        }
+
         let Some(name) = elf.strtab.get_at(sym.st_name) else {
             continue;
         };
 
-        if missing_symbols.contains(&name) {
-            let offset = elf.syms.offset() + index * Sym::size_with(elf.syms.ctx());
-            let real_addr = kernel_symbols
-                .get(name).ok_or(anyhow::anyhow!("Cannot found symbol: {}", &name))?;
-            sym.st_shndx = section_header::SHN_ABS as usize;
-            sym.st_value = *real_addr;
-            modifications.push((sym, offset));
-        }
+        let offset = elf.syms.offset() + index * Sym::size_with(elf.syms.ctx());
+        let real_addr = kernel_symbols
+            .get(name).ok_or(anyhow::anyhow!("Cannot found symbol: {}", &name))?;
+        sym.st_shndx = section_header::SHN_ABS as usize;
+        sym.st_value = *real_addr;
+        modifications.push((sym, offset));
     }
-
-    anyhow::ensure!(
-        modifications.len() == missing_symbols.len(),
-        "Missing symbols!"
-    );
 
     let ctx = *elf.syms.ctx();
     for ele in modifications {
