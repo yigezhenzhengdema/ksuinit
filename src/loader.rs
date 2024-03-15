4b638fc720645755cd45188516811067c1c59115
weishu <twsxtd@gmail.com>
2024-03-15 13:22:44 +0800

    trim llvm symbols

--- src/loader.rs
+++ src/loader.rs
@@ -40,7 +40,7 @@ fn parse_kallsyms() -> Result<HashMap<String, u64>> {
         .map(|(symbol, addr)| {
             (
                 symbol
-                    .find(['$'])
+                    .find("$").or_else(|| symbol.find(".llvm."))
                     .map_or(symbol, |pos| &symbol[0..pos])
                     .to_owned(),
                 addr,
